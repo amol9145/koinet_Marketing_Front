f244de9756fd0d23b4163c65c7b46c6d29ccf350
amol9145 <amol@koinetmedia.com>
2024-12-05 13:44:45 +0530

    ddddddddd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // Make sure your paths are correct for all files
+        "./src/**/*.{js,jsx,ts,tsx}", // Make sure your paths are correct for all files
     ],
     theme: {
-      extend: {
-        animation: {
-          gradient: "gradient-bg 6s ease infinite", // Define the animation
+        extend: {
+            animation: {
+                gradient: "gradient-bg 6s ease infinite", // Define the animation
+            },
+            keyframes: {
+                "gradient-bg": {
+                    "0%": { backgroundPosition: "0% 50%" },
+                    "50%": { backgroundPosition: "100% 50%" },
+                    "100%": { backgroundPosition: "0% 50%" },
+                },
+            },
+            backgroundSize: {
+                "200%": "200% 200%", // Ensure smooth gradient transitions
+            },
         },
-        keyframes: {
-          "gradient-bg": {
-            "0%": { backgroundPosition: "0% 50%" },
-            "50%": { backgroundPosition: "100% 50%" },
-            "100%": { backgroundPosition: "0% 50%" },
-          },
-        },
-        backgroundSize: {
-          "200%": "200% 200%", // Ensure smooth gradient transitions
-        },
-      },
     },
     plugins: [],
-  };
-  
-
+};
