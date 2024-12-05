762e0e1dee5c5ae9459a8a5cf7d2670edc0c5eca
amol9145 <amol@koinetmedia.com>
2024-12-05 15:16:22 +0530

    dsdsaads

--- src/Components/HeadNavbar.jsx
+++ src/Components/HeadNavbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-white shadow-lg border-gray-200 dark:border-gray-600 dark:bg-gray-900 fixed top-0 z-50 w-full">
+        <nav className="bg-white shadow-lg border-gray-200 dark:border-gray-600  fixed top-0 z-50 w-full">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                 <Link to={"/"} className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img
@@ -53,11 +53,11 @@ const Navbar = () => {
                     className={`items-center justify-between font-medium ${isMenuOpen ? "flex" : "hidden"
                         } w-full md:flex md:w-auto md:order-1`}
                 >
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white  dark:border-gray-700">
                         <li>
                             <Link
                                 to={"/"}
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500  dark:hover:text-blue-500  dark:border-gray-700"
                             >
                                 Home
                             </Link>
@@ -65,7 +65,7 @@ const Navbar = () => {
                         <li>
                             <Link
                                 to={"/latest_reports"}
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500  dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                             >
                                 Latest Reports
                             </Link>
