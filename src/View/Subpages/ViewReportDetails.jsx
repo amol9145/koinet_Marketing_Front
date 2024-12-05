f948e2deba955c2a4215dccf32dd0ded36fb623d
amol9145 <amol@koinetmedia.com>
2024-12-05 15:11:50 +0530

    assss

--- src/View/Subpages/ViewReportDetails.jsx
+++ src/View/Subpages/ViewReportDetails.jsx
@@ -356,7 +356,7 @@ function ViewReportDetails() {
                                     REPORT BUYING OPTIONS
                                 </p>
                                 <div className="mt-5">
-                                    <ul className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ">
+                                    <ul className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg  dark:border-gray-600 dark:text-white transition-all duration-300 ease-in-out hover:bg-gray-100 ">
                                         <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                                             <div className="flex items-center ps-3">
                                                 <input
