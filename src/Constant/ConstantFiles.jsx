7146b81da50d9b5a0b490c047ebf3589606c6f55
amol9145 <amol@koinetmedia.com>
2024-12-05 15:40:24 +0530

    dsadssda

--- src/Constant/ConstantFiles.jsx
+++ src/Constant/ConstantFiles.jsx
@@ -1,6 +1,6 @@
 const TimceApi = "alkwa3htp0d7bnfkqhmln5marvfeuzjnfofiwyvldq4g0gd9";
-const baseUrl = "http://localhost:3000";
-// const baseUrl = "https://marketing-koinet-backend-research.onrender.com";
+// const baseUrl = "http://localhost:3000";
+const baseUrl = "https://marketing-koinet-backend-research.onrender.com";
 
 
 export { TimceApi, baseUrl };
