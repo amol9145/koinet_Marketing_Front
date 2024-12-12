42995e9bd02a1d74601c207522a35d25616ae459
amol9145 <amol@koinetmedia.com>
2024-12-12 17:02:42 +0530

    dsdsdsf

--- src/Dashboard/HomeDashboard.jsx
+++ src/Dashboard/HomeDashboard.jsx
@@ -20,7 +20,7 @@ const HomeDashboard = () => {
                         onClick={() => setActiveTab('dashboard')}
                         className={`inline-flex items-center px-4 py-3 rounded-lg w-full ${activeTab === 'dashboard'
                             ? 'text-gray-900 bg-gray-100 dark:bg-gray-700 dark:text-white'
-                            : 'hover:text-gray-900 bg-gray-50 hover:bg-gray-100   text-black'
+                            : 'hover:text-gray-900 bg-gray-50 hover:bg-gray-100 text-black'
                             }`}
                     >
                         Dashboard
@@ -79,27 +79,27 @@ const HomeDashboard = () => {
             {/* Tab content */}
             <div className="w\-full"> {/* Full width applied here */}
                 {activeTab === 'dashboard' && (
-                    <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 w-full">
+                    <div className="p-6 bg-gray-50 text-medium text-gray-500   w-full">
                         <MainDashboardBack />
                     </div>
                 )}
                 {activeTab === 'uploadReport' && (
-                    <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 w-full">
+                    <div className="p-6 bg-gray-50 text-medium text-gray-500   w-full">
                         <CreateReports />
                     </div>
                 )}
                 {activeTab === 'uploadPressRelease' && (
-                    <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 w-full">
+                    <div className="p-6 bg-gray-50 text-medium text-gray-500   w-full">
                         <CreatePressRelease />
                     </div>
                 )}
                 {activeTab === 'uploadInfographics' && (
-                    <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 w-full">
+                    <div className="p-6 bg-gray-50 text-medium text-gray-500   w-full">
                         <CreateInfographics />
                     </div>
                 )}
                 {activeTab === 'allOrders' && (
-                    <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 w-full">
+                    <div className="p-6 bg-gray-50 text-medium text-gray-500   w-full">
                         <OrdersList />
                     </div>
                 )}
