2c662719de3e593334a754e0f72ab3d0052333ee
amol9145 <amol@koinetmedia.com>
2024-12-16 14:31:47 +0530

    dsddsa

--- src/View/Subpages/ViewReportDetails.jsx
+++ src/View/Subpages/ViewReportDetails.jsx
@@ -443,8 +443,8 @@ function ViewReportDetails() {
                                         </li>
                                     </ul>
 
-                                    <button onClick={handlePayment} className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                                        <span className="  relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    <button onClick={handlePayment} className="mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                                        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-black bg-white  rounded-md group-hover:bg-opacity-0">
                                             Buy Now
                                         </span>
                                     </button>
@@ -459,7 +459,6 @@ function ViewReportDetails() {
                                 <div>
                                     {/* Button to open the modal */}
                                     <div className="mb-1">
-
                                         <Link onClick={handleOpenModal} className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
                                             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-black bg-white  rounded-md group-hover:bg-opacity-0">
                                                 Download Sample Report
