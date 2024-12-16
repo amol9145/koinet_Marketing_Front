57639bdac41085ce2df9a7b73e3f7992cbf06751
amol9145 <amol@koinetmedia.com>
2024-12-16 14:30:22 +0530

    dsdassda

--- src/View/Subpages/ViewReportDetails.jsx
+++ src/View/Subpages/ViewReportDetails.jsx
@@ -458,7 +458,7 @@ function ViewReportDetails() {
 
                                 <div>
                                     {/* Button to open the modal */}
-                                    <div className="mb-4">
+                                    <div className="mb-1">
 
                                         <Link onClick={handleOpenModal} className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
                                             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-black bg-white  rounded-md group-hover:bg-opacity-0">
@@ -468,7 +468,7 @@ function ViewReportDetails() {
                                     </div>
 
                                     {/* Modal */}
-                                    <div className="mb-4">
+                                    <div className="mb-1">
                                         <Link to={"/contact"} className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
                                             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-black bg-white  rounded-md group-hover:bg-opacity-0">
                                                 Request Customization
@@ -476,7 +476,7 @@ function ViewReportDetails() {
                                         </Link>
                                     </div>
 
-                                    <div className="mb-4">
+                                    <div className="mb-1">
                                         <Link to={"/contact"} className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
                                             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-black bg-white  rounded-md group-hover:bg-opacity-0">
                                                 Speak to Consultant
