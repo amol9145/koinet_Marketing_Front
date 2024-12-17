509a8e0b9dfdeaafb02fb3c342aec257027f5617
amol9145 <amol@koinetmedia.com>
2024-12-17 15:32:51 +0530

    dssda

--- src/View/footer/Contact.jsx
+++ src/View/footer/Contact.jsx
@@ -288,8 +288,11 @@ function Contact() {
 
                   {/* Submit Button */}
                   <div className="text-center">
-                    <button type="submit" className="mt-3 inline-flex items-center justify-center p-3 text-sm font-medium text-white bg-gradient-to-r from-blue-500 to-indigo-500 rounded-lg hover:bg-gradient-to-l">
-                      Submit
+
+                    <button type="submit" className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                      <span className=" text-black relative px-5 py-2.5 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
+                        Submit
+                      </span>
                     </button>
                   </div>
                 </form>
