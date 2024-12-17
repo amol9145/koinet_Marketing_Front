b5fe07243e05a02625b7f19a1e04b4f2b47569f2
amol9145 <amol@koinetmedia.com>
2024-12-17 09:21:27 +0530

    ddf

--- src/View/Subpages/ViewInfographics.jsx
+++ src/View/Subpages/ViewInfographics.jsx
@@ -424,8 +424,8 @@ function ViewInfographics() {
                                     {/* Button to open the modal */}
                                     <div className="mb-4">
 
-                                        <Link onClick={handleOpenModal} className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                        <Link onClick={handleOpenModal} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400  focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-whitetext-black rounded-md group-hover:bg-opacity-0">
                                                 Download Sample Report
                                             </span>
                                         </Link>
@@ -433,15 +433,15 @@ function ViewInfographics() {
 
                                     {/* Modal */}
                                     <div className="mb-4">
-                                        <Link to={"/contact"} className="  relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                        <Link to={"/contact"} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400  focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-whitetext-black rounded-md group-hover:bg-opacity-0">
                                                 Request Customization
                                             </span>
                                         </Link>
                                     </div>
                                     <div className="mb-4">
-                                        <Link to={"/contact"} className="  relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                        <Link to={"/contact"} className=" relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400  focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-whitetext-black rounded-md group-hover:bg-opacity-0">
                                                 Speak to Consultant
                                             </span>
                                         </Link>
