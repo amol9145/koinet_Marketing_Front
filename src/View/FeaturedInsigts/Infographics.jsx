9a0e9006a1679f7fcbf338a3de5aa487490e5a56
amol9145 <amol@koinetmedia.com>
2024-12-18 09:19:57 +0530

    df

--- src/View/FeaturedInsigts/Infographics.jsx
+++ src/View/FeaturedInsigts/Infographics.jsx
@@ -114,18 +114,50 @@ function Infographics() {
                                     <Link to={`/infographics/viewInfographics/${item._id}`}>
                                         <p className="mb-2 text-black text-sm sm:text-base">{item.category}</p>
                                     </Link>
-                                    <div className="flex items-center justify-center flex-wrap gap-2">
+                                    <div className="flex text-center items-center justify-center">
                                         <button
                                             type="button"
-                                            className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-4 py-2 text-center flex items-center"
+                                            className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2"
                                         >
                                             <svg
-                                                className="w-4 h-4 mr-2"
+                                                className="w-4 h-4 me-2"
+                                                aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 fill="currentColor"
                                                 viewBox="0 0 8 19"
                                             >
                                                 <path d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" />
+                                            </svg>
+                                            share
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="text-white bg-[#0077B5] hover:bg-[#0077B5]/90 focus:ring-4 focus:outline-none focus:ring-[#0077B5]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#0077B5]/55 me-2 mb-2"
+                                        >
+                                            <svg
+                                                className="w-4 h-4 me-2"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
+                                                viewBox="0 0 448 512"
+                                            >
+                                                <path d="M100.28 448H7.4V149.5h92.88zm-46.44-340a53.37 53.37 0 1 1 53.44-53.37A53.37 53.37 0 0 1 53.84 108zM447.8 448h-92.68V302.4c0-34.7-.7-79.2-48.2-79.2-48.3 0-55.7 37.7-55.7 76.6V448h-92.6V149.5h88.9v40.8h1.3c12.4-23.5 42.5-48.3 87.3-48.3 93.4 0 110.5 61.5 110.5 141.3V448z" />
+                                            </svg>
+                                            Share
+                                        </button>
+
+                                        <button
+                                            type="button"
+                                            className="text-white bg-[#E1306C] hover:bg-[#E1306C]/90 focus:ring-4 focus:outline-none focus:ring-[#E1306C]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#E1306C]/55 me-2 mb-2"
+                                        >
+                                            <svg
+                                                className="w-4 h-4 me-2"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
+                                                viewBox="0 0 448 512"
+                                            >
+                                                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9S160.5 370.9 224.1 370.9 339 319.6 339 255.9 287.7 141 224.1 141zm0 186.6c-39.6 0-71.7-32.1-71.7-71.7s32.1-71.7 71.7-71.7 71.7 32.1 71.7 71.7-32.1 71.7-71.7 71.7zM370.5 136.1c0 14.9-12.1 27-27 27s-27-12.1-27-27 12.1-27 27-27 27 12.1 27 27zM398.8 76.5C375.7 53.4 345.8 40 311.8 40H136.2c-34 0-63.9 13.4-87 36.5-23.1 23.1-35.8 52.9-35.8 84.7v204.2c0 31.8 12.7 61.6 35.8 84.7 23.1 23.1 53 36.5 87 36.5h175.6c34 0 63.9-13.4 87-36.5 23.1-23.1 35.8-52.9 35.8-84.7V161.2c0-31.8-12.7-61.6-35.8-84.7z" />
                                             </svg>
                                             Share
                                         </button>
