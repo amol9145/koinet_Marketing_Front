c0333b722e18368b7eef4e1c428ac471fa8897fb
amol9145 <amol@koinetmedia.com>
2024-12-18 10:06:01 +0530

    cxccx

--- src/View/Subpages/ViewInfographics.jsx
+++ src/View/Subpages/ViewInfographics.jsx
@@ -197,13 +197,13 @@ function ViewInfographics() {
                                             required
                                         ></textarea>
                                     </div>
-
-                                    <button
-                                        type="submit"
-                                        className="w-full text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center sm:me-2 sm:mb-2"
-                                    >
-                                        Download Sample Report
-                                    </button>
+                                    <div className="flex justify-center">
+                                        <button type="submit" className=" mt-3 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+                                            <span className=" text-black relative px-5 py-2.5 transition-all ease-in duration-75 bg-white  rounded-md group-hover:bg-opacity-0">
+                                                Download Sample Report
+                                            </span>
+                                        </button>
+                                    </div>
                                 </form>
                             </div>
                         </div>
